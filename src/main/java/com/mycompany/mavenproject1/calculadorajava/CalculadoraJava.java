7dd6f0352dbb610a552d99ad134a606a80ec753b
Mario Leandro Silva Marques <marioleandro2004@gmail.com>
2023-12-12 19:19:59 -0300

    Primeiro projeto em java

---
+++ src/main/java/com/mycompany/mavenproject1/calculadorajava/CalculadoraJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1.calculadorajava;
+
+/**
+ *
+ * @author mario
+ */
+public class CalculadoraJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
